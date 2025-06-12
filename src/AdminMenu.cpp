0c49aeb3e95d8a72700185e3af6fb419cf147a5f
PisethMao <pisethmao2002@gmail.com>
2025-06-12 17:02:09 +0700

    validate all feature include excel and do search/filter feature for admin

--- src/AdminMenu.cpp
+++ src/AdminMenu.cpp
@@ -6,6 +6,22 @@
 #include <limits>
 using namespace std;
 using namespace tabulate;
+void pressEnter()
+{
+    Table pressTable;
+    pressTable.add_row({"Press Enter to continue..."});
+    pressTable.format()
+        .font_align(FontAlign::center)
+        .font_style({FontStyle::bold})
+        .border_top("-")
+        .border_bottom("-")
+        .border_left("|")
+        .border_right("|")
+        .corner("+");
+    cout << pressTable << endl;
+    cin.ignore();
+    cin.get();
+}
 void showAdminMenu(StockManager &stockManager)
 {
     while (true)
@@ -88,24 +104,102 @@ void showAdminMenu(StockManager &stockManager)
                 .corner("+");
             cout << displayTable << endl;
             stockManager.displayData();
-            Table pressTable;
-            pressTable.add_row({"Press Enter to continue..."});
-            pressTable.format()
-                .font_align(FontAlign::center)
-                .font_style({FontStyle::bold})
+            pressEnter();
+            break;
+        }
+        case 3:
+        {
+            system("cls");
+            int subChoice;
+            Table searchAndFilterMenu;
+            searchAndFilterMenu.add_row({"===============[ << Search & Filter Menu >> ]==============="});
+            searchAndFilterMenu.add_row({"1. Search by ID"});
+            searchAndFilterMenu.add_row({"2. Search by Type"});
+            searchAndFilterMenu.add_row({"3. Search by Brand"});
+            searchAndFilterMenu.add_row({"4. Search by Model"});
+            searchAndFilterMenu.add_row({"5. Filter by Year"});
+            searchAndFilterMenu.add_row({"6. Filter by Origin"});
+            searchAndFilterMenu.add_row({"7. Filter by Price"});
+            searchAndFilterMenu.add_row({"8. Exit Case Search & Filter Menu"});
+            searchAndFilterMenu.format()
+                .font_align(FontAlign::left)
                 .border_top("-")
                 .border_bottom("-")
                 .border_left("|")
                 .border_right("|")
                 .corner("+");
-            cout << pressTable << endl;
-            cin.ignore();
-            cin.get();
-            break;
+            cout << searchAndFilterMenu << endl;
+            while (true)
+            {
+                cout << "Choose an option [1-8]: ";
+                if (cin >> subChoice && subChoice >= 1 && subChoice <= 8)
+                {
+                    break;
+                }
+                else
+                {
+                    Table invalidTable;
+                    invalidTable.add_row({"Invalid input! Please enter a number from 1 to 8."});
+                    invalidTable.format()
+                        .font_align(FontAlign::center)
+                        .font_style({FontStyle::bold})
+                        .border_top("-")
+                        .border_bottom("-")
+                        .border_left("|")
+                        .border_right("|")
+                        .corner("+");
+                    cout << invalidTable << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    continue;
+                }
+            }
+            switch (subChoice)
+            {
+            case 1:
+            {
+                int id;
+                while (true)
+                {
+                    cout << "Enter ID to search: ";
+                    if ((cin >> id) && id > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Table errorTable;
+                        errorTable.add_row({"Invalid ID! Please enter a positive number only."});
+                        errorTable.format()
+                            .font_align(FontAlign::center)
+                            .font_style({FontStyle::bold})
+                            .border_top("-")
+                            .border_bottom("-")
+                            .border_left("|")
+                            .border_right("|")
+                            .corner("+");
+                        cout << errorTable << endl;
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    }
+                }
+                // stockManager.loadDataFromFile();
+                stockManager.searchById(id);
+                break;
+            }
+            case 2:
+            {
+                string type;
+                cout << "Enter type to search: ";
+                getline(cin >> ws, type);
+                // stockManager.searchByType(id);
+                break;
+            }
+            default:
+                break;
+            }
+            pressEnter();
         }
-        // case 3:
-        //     stockManager.searchAndFilter();
-        //     break;
         // case 4:
         //     stockManager.updateRecord();
         //     break;
