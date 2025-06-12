e67174d15d1e9fb622990f58e586a1d5b60a799b
PisethMao <pisethmao2002@gmail.com>
2025-06-12 19:56:43 +0700

    fixed error code

--- src/Logout.cpp
+++ src/Logout.cpp
@@ -1,27 +1,27 @@
-#include "Logout.hpp"
-#include "Auth.hpp"
-#include "CustomerMenu.hpp"
-#include <algorithm>
-#include <iostream>
-using namespace std;
-void clearScreen ()
-{
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
-}
-void logout ()
-{
-    clearScreen ();
-    string choice;
-    cout<<"Are you sure you want to logout? (yes or no): ";
-    getline(cin >> ws, choice);
-    if (choice == "yes" || choice == "Yes" || choice == "y" || choice == "Y") {
-        clearScreen ();
-        cout << "\t=>You are successfully logged out!!" << endl;
-    } else {
-        showCustomerMenu ();
-    }
-}
+// #include "Logout.hpp"
+// #include "Auth.hpp"
+// #include "CustomerMenu.hpp"
+// #include <algorithm>
+// #include <iostream>
+// using namespace std;
+// void clearScreen ()
+// {
+//     #ifdef _WIN32
+//         system("cls");
+//     #else
+//         system("clear");
+//     #endif
+// }
+// void logout ()
+// {
+//     clearScreen ();
+//     string choice;
+//     cout<<"Are you sure you want to logout? (yes or no): ";
+//     getline(cin >> ws, choice);
+//     if (choice == "yes" || choice == "Yes" || choice == "y" || choice == "Y") {
+//         clearScreen ();
+//         cout << "\t=>You are successfully logged out!!" << endl;
+//     } else {
+//         showCustomerMenu ();
+//     }
+// }
